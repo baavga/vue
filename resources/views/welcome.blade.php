ef01dd3caa24f4ee7f808a6aea1132baeaa806da
jani <b.jani93@yahoo.com>
2018-05-26 19:54:45 +0800

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/app.css">
 
      
     </head>
@@ -14,7 +15,7 @@
             <div class="container">
                 <articles></articles>
             </div>
-        </div>
+        </div>  
         <script src={{asset("js/app.js")}}></script>
     </body>
 </html>
